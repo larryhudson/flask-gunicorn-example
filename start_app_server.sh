fee4485e1fc509ebae8a60c8ea08d31e1f55d9b5
Larry Hudson <larryhudson@hey.com>
2023-04-22 12:37:00 +1000

    tweaking

--- start_app_server.sh
+++ start_app_server.sh
@@ -1,12 +1,19 @@
-. ./output_style.config
+Reset='\e[0m'
+Info='\e[1;34m'
+Start='\e[1;32m'
+Stop='\e[1;33m'
+Link='\e[7m'
+ResetLink='\e[27m'
 
-. ./app_server.config
+APP_HOST=0.0.0.0
+APP_PORT=4400
+APP_WORKERS=2
 
 echo -e "${Info}Activate the virtual environment${Reset}"
-. venv/bin/activate
+source venv/bin/activate
 
 echo -e "${Info}Install or update python modules${Reset}"
-pip3 install --upgrade -r requirements.txt
+pip3 install -r requirements.txt
 
 echo -e "${Start}Start application server at ${Link}http://${APP_HOST}:${APP_PORT}${ResetLink} with ${APP_WORKERS} workers${Reset}"
 gunicorn -b $APP_HOST:$APP_PORT -w=$APP_WORKERS wsgi:app
